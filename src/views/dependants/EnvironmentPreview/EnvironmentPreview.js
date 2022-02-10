d98479044a183f0d8bb4585257ca2917207c9230
izuru775 <izuru775@gmail.com>
2022-02-10 22:42:31 +1100

    Major bug fix

--- src/views/dependants/EnvironmentPreview/EnvironmentPreview.js
+++ src/views/dependants/EnvironmentPreview/EnvironmentPreview.js
@@ -32,9 +32,17 @@ export const EnvironmentPreview = () => {
     },
     [isMounted]
   );
-
+  /**
+   * Four Environment Ids for demo
+   * 
+   * 360 video : 6204451d9bfe7f1ea9cd50e4
+   * 360 image : 6204f53c06e56139b4cde0f9
+   * Preset Environment : 6204f75706e56139b4cde116
+   * Default Environment : 6204f8de06e56139b4cde13e
+   * 
+   */
   useEffect(() => {
-    getEnvironmentById("6204451d9bfe7f1ea9cd50e4");
+    getEnvironmentById("6204f8de06e56139b4cde13e");
   }, [getEnvironmentById]);
 
   console.log(objects);
@@ -117,65 +125,87 @@ export const EnvironmentPreview = () => {
 
                 </Scene>
               ))
-              : environment.map((data) => (
-                <Scene
-                  key={data._id}
-                  id="mainScene"
-                  background="color:black"
-                  className="menu"
-                >
-                  {environment ? (
-                    environment.map((data) => (
-                      <a-sky key={data._id} color={data.skyColor}></a-sky>
-                    ))
-                  ) : (
-                    <a-sky></a-sky>
-                  )}
-
-                  {environment ? (
-                    environment.map((data) => (
-                      <a-plane
-                        key={data._id}
-                        id="plane"
-                        position="0 0 -4"
-                        rotation="-90 0 0"
-                        scale="100 100 100"
-                        width="100"
-                        height="100"
-                        color={data.floorColor}
-                      ></a-plane>
-                    ))
-                  ) : (
-                    <a-plane></a-plane>
-                  )}
-                  <a-assets>
-                    {objects ? (
-                      objects.map((data) => (
-                        <a-asset-item
-                          key={data._id}
-                          id={data.objectName}
-                          src={data.url}
-                        ></a-asset-item>
+              : environment.map((data) =>
+                data.panorama ? environment.map((data) => (
+                  <Scene
+                    key={data._id}
+                    id="mainScene"
+                    background="color:black"
+                    className="menu"
+                  >
+                    {environment ? (
+                      environment.map((data,i) => (
+                        <a-sky key={i} id="sky" src={data.skyUrl} rotation="0 -130 0"></a-sky>
                       ))
                     ) : (
-                      <a-asset-item></a-asset-item>
+                      <a-sky></a-sky>
                     )}
-                  </a-assets>
-                  {objects ? (
-                    objects.map((data) => (
-                      <a-gltf-model
-                        key={data._id}
-                        src={"#" + data.objectName}
-                        position={data.position}
-                        scale={data.scale}
-                        rotation={data.rotation}
-                      ></a-gltf-model>
-                    ))
-                  ) : (
-                    <a-gltf-model></a-gltf-model>
-                  )}
-                </Scene>
-              ))
+
+                    
+                    <a-assets>
+                      
+                    </a-assets>
+                    
+                  </Scene>
+                )) : environment.map((data) => (
+                  <Scene
+                    key={data._id}
+                    id="mainScene"
+                    background="color:black"
+                    className="menu"
+                  >
+                    {environment ? (
+                      environment.map((data) => (
+                        <a-sky key={data._id} color={data.skyColor}></a-sky>
+                      ))
+                    ) : (
+                      <a-sky></a-sky>
+                    )}
+
+                    {environment ? (
+                      environment.map((data) => (
+                        <a-plane
+                          key={data._id}
+                          id="plane"
+                          position="0 0 -4"
+                          rotation="-90 0 0"
+                          scale="100 100 100"
+                          width="100"
+                          height="100"
+                          color={data.floorColor}
+                        ></a-plane>
+                      ))
+                    ) : (
+                      <a-plane></a-plane>
+                    )}
+                    <a-assets>
+                      {objects ? (
+                        objects.map((data) => (
+                          <a-asset-item
+                            key={data._id}
+                            id={data.objectName}
+                            src={data.url}
+                          ></a-asset-item>
+                        ))
+                      ) : (
+                        <a-asset-item></a-asset-item>
+                      )}
+                    </a-assets>
+                    {objects ? (
+                      objects.map((data) => (
+                        <a-gltf-model
+                          key={data._id}
+                          src={"#" + data.objectName}
+                          position={data.position}
+                          scale={data.scale}
+                          rotation={data.rotation}
+                        ></a-gltf-model>
+                      ))
+                    ) : (
+                      <a-gltf-model></a-gltf-model>
+                    )}
+                  </Scene>
+                )))
           )
       )}
     </>
